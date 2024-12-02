6e93cc393e393ac7f22c0dfcb68104d53a620af1
Ed Charbeneau <ed.charbeneau@gmail.com>
2024-12-02 11:52:00 -0500

    Add project files.

---
+++ Pages/ChatModels.cs
@@ -0,0 +1,31 @@
+﻿namespace WebLLM.Pages;
+public class InitProgress
+{
+	public float? Progress { get; set; }
+	public string Text { get; set; } = "";
+}
+
+public class WebLLMCompletion
+{
+	public string? Id { get; set; }
+	public string? Object { get; set; }
+	public string? Model { get; set; }
+	public string? SystemFingerprint { get; set; }
+	public Choice[]? Choices { get; set; }
+}
+
+public class Choice
+{
+	public int? Index { get; set; }
+	public Message? Message { get; set; }
+	public object? Logprobs { get; set; }
+	public string? FinishReason { get; set; }
+}
+
+public class Message
+{
+	public string Role { get; set; } = "user";
+	public string Content { get; set; } = "";
+}
+
+
---
+++ Pages/WebLLMService.cs
@@ -0,0 +1,39 @@
+﻿namespace WebLLM.Pages;
+
+using Microsoft.JSInterop;
+public class WebLLMService
+{
+    private string model = "Llama-3.2-1B-Instruct-q4f16_1-MLC";
+	private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+	
+	//private const string ModulePath = "https://edcharbeneau.com/javascript/webllm-interop.js";
+	private const string ModulePath = "./webllm-interop.js";
+	public WebLLMService(IJSRuntime jsRuntime)
+	{
+		moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+		"import", $"{ModulePath}").AsTask());
+	}
+
+	public async Task Initialize()
+	{
+		var module = await moduleTask.Value;
+		await module.InvokeVoidAsync("initialize", model, DotNetObjectReference.Create(this));
+	}
+
+	public event Action<InitProgress>? OnInitializingChanged;
+
+	[JSInvokable]
+	public Task OnInitializing(InitProgress status)
+	{
+		OnInitializingChanged?.Invoke(status);
+		return Task.CompletedTask;
+	}
+
+	public async Task<WebLLMCompletion> CompleteAsync(IList<Message> messages)
+	{
+		var module = await moduleTask.Value;
+		return await module.InvokeAsync<WebLLMCompletion>("complete", messages);
+	}
+
+}
+
